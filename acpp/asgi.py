001ea29f262105a603382e28dd02956688cb96a8
jiajun li <2453171786@qq.com>
2022-01-21 16:36:47 +0800

    new_project

---
+++ acpp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for acpp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acpp.settings')
+
+application = get_asgi_application()
