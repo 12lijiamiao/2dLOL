c3e4425358458e3bd63aa29a53983c6b8cbf307f
jiajun li <2453171786@qq.com>
2022-01-21 18:17:27 +0800

    suecess play_server

--- acpp/settings.py
+++ acpp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o-_5#g^sgq9i@13o^_^#e28db8=i^t7zmur#t53fz@u2*$4g_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["39.106.69.7"]
 
 
 # Application definition
