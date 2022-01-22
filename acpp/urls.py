c55c493ff5ad2ea3eb3688d587f4a65623a6fcb8
jiajun li <2453171786@qq.com>
2022-01-22 22:42:45 +0800

    finish page setting

--- acpp/urls.py
+++ acpp/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-  #  path('name/',include('game.urls')),#urls
+    path('',include('game.urls.index')),#urls
     path('admin/', admin.site.urls),
 ]
