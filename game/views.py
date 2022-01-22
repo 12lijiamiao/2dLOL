815523c984680c1ad65057297a60596482ba3584
jiajun li <2453171786@qq.com>
2022-01-22 21:54:25 +0800

    change structure

--- game/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line = '<h1 style="text-align :center">Test<h1>'
-    return HttpResponse(line)
---
+++ scripts/create_dist_game_js.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+PATH_STATIC=/home/ljm/acpp/game/static/js/
+PATH_STATIC_DIST=${PATH_STATIC}dist/
+PATH_STATIC_SRC=${PATH_STATIC}src/
+
+find ${PATH_STATIC_SRC} -type f -name '*.js' | sort | xargs cat > ${PATH_STATIC_DIST}game.css
